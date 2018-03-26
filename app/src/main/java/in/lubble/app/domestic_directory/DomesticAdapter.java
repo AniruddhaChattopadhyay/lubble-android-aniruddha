08c50a0e80c94dd48b1022a45927c6c21ab565d3
Ishaan Garg <ishaangarg94@gmail.com>
2018-03-26 23:47:28 +0530

    Added generated avatars to list of domestic helps

--- app/src/main/java/in/lubble/app/domestic_directory/DomesticAdapter.java
+++ app/src/main/java/in/lubble/app/domestic_directory/DomesticAdapter.java
@@ -2,6 +2,8 @@ package in.lubble.app.domestic_directory;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -57,6 +59,9 @@ public class DomesticAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         } else {
             final DomesticViewHolder domesticViewHolder = (DomesticViewHolder) holder;
             domesticViewHolder.nameTv.setText(domesticHelpData.getName());
+            domesticViewHolder.initialTv.setText(String.valueOf(domesticHelpData.getName().charAt(0)));
+            domesticViewHolder.avatarIcon.setColorFilter(new
+                    PorterDuffColorFilter(getVariableColor(position), PorterDuff.Mode.SRC_IN));
         }
     }
 
@@ -66,6 +71,25 @@ public class DomesticAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         notifyDataSetChanged();
     }
 
+    private int getVariableColor(int position) {
+        int selector = position % 5;
+        switch (selector) {
+            case 0:
+                return context.getResources().getColor(R.color.dk_cyan);
+            case 1:
+                return context.getResources().getColor(R.color.dk_red);
+            case 2:
+                return context.getResources().getColor(R.color.mute_yellow);
+            case 3:
+                return context.getResources().getColor(R.color.mute_purple);
+            case 4:
+                return context.getResources().getColor(R.color.dk_green);
+            default:
+                return context.getResources().getColor(R.color.mute_orange);
+        }
+    }
+
+
     @Override
     public int getItemCount() {
         return domesticList.size();
@@ -74,13 +98,17 @@ public class DomesticAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
     class DomesticViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         final View mView;
         final TextView nameTv;
+        final TextView initialTv;
         final ImageView callIcon;
+        final ImageView avatarIcon;
 
         DomesticViewHolder(View view) {
             super(view);
             mView = view;
             nameTv = view.findViewById(R.id.tv_name);
             callIcon = view.findViewById(R.id.iv_call);
+            avatarIcon = view.findViewById(R.id.iv_avatar);
+            initialTv = view.findViewById(R.id.tv_name_initial);
             callIcon.setOnClickListener(this);
         }
 
