e78d3de4901433af158146908a722f2303c29118
ishaangarg <ishaan.garg@shadowfax.in>
2018-07-10 22:13:53 +0530

    started mplace

---
+++ app/src/main/java/in/lubble/app/marketplace/MarketplaceActivity.java
@@ -0,0 +1,16 @@
+package in.lubble.app.marketplace;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import in.lubble.app.R;
+
+public class MarketplaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marketplace);
+    }
+
+}
