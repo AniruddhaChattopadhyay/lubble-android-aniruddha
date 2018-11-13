e697d00971c8d357954f43c48235f2109cce4226
Ishaan Garg <ishaangarg94@gmail.com>
2018-11-13 16:01:07 +0530

    open graph title and desc is fetched first

--- app/src/main/java/in/lubble/app/network/LinkMetaAsyncTask.java
+++ app/src/main/java/in/lubble/app/network/LinkMetaAsyncTask.java
@@ -1,8 +1,10 @@
 package in.lubble.app.network;
 
 import android.os.AsyncTask;
+import android.text.TextUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
@@ -26,8 +28,22 @@ public class LinkMetaAsyncTask extends AsyncTask<Void, Void, Void> {
     protected Void doInBackground(Void... params) {
         try {
             Document doc = Jsoup.connect(url).timeout(30 * 1000).get();
-            String title = doc.title();
-            String description = doc.select("meta[name=description]").attr("content");
+
+            String title = "";
+            Elements metaOgTitle = doc.select("meta[property=og:title]");
+            if (metaOgTitle != null && !TextUtils.isEmpty(metaOgTitle.attr("content"))) {
+                title = metaOgTitle.attr("content");
+            } else {
+                title = doc.title();
+            }
+
+            Elements metaOgDesc = doc.select("meta[name=og:description]");
+            String description = "";
+            if (metaOgDesc != null && !TextUtils.isEmpty(metaOgDesc.attr("content"))) {
+                description = metaOgDesc.attr("content");
+            } else {
+                description = doc.select("meta[name=description]").attr("content");
+            }
             listener.onMetaFetched(title, description);
         } catch (IOException e) {
             e.printStackTrace();
