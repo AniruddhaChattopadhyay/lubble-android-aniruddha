ee1f78f78bfdfc6a473cd154d006b61a7ce7a3ee
Ishaan Garg <ishaangarg94@gmail.com>
2019-12-12 20:57:56 +0530

    keyboard no longer hides the INVITE TO CHAT btn when crating new DM

--- app/src/main/java/in/lubble/app/profile/DmIntroBottomSheet.java
+++ app/src/main/java/in/lubble/app/profile/DmIntroBottomSheet.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.button.MaterialButton;
@@ -36,6 +37,12 @@ public class DmIntroBottomSheet extends BottomSheetDialogFragment {
         DmIntroBottomSheet fragment = new DmIntroBottomSheet();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogStyle);
     }
 
     @Nullable
