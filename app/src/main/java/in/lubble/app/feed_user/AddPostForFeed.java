1ba328be56b8c4630509af8c092db74995d2c5b7
Ishaan Garg <ishaangarg94@gmail.com>
2021-04-27 20:21:05 +0530

    new post UI changes

--- app/src/main/java/in/lubble/app/feed_user/AddPostForFeed.java
+++ app/src/main/java/in/lubble/app/feed_user/AddPostForFeed.java
@@ -2,18 +2,25 @@ package in.lubble.app.feed_user;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 import in.lubble.app.BaseActivity;
 import in.lubble.app.GlideApp;
+import in.lubble.app.LubbleSharedPrefs;
 import in.lubble.app.R;
 import in.lubble.app.models.FeedPostData;
 
 public class AddPostForFeed extends BaseActivity {
+
+    private static final int REQ_CODE_GROUPS_SELECT = 853;
 
     private Button postSubmitBtn;
     private EditText postText;
@@ -24,6 +31,11 @@ public class AddPostForFeed extends BaseActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_add_post_for_feed);
+
+        Toolbar toolbar = findViewById(R.id.text_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("Post to " + LubbleSharedPrefs.getInstance().getLubbleName());
 
         postSubmitBtn = findViewById(R.id.post_btn);
         postText = findViewById(R.id.post_edt_txt);
@@ -46,9 +58,27 @@ public class AddPostForFeed extends BaseActivity {
 
     private void openGroupSelectionActivity(FeedPostData feedPostData) {
         Intent groupSelectionActivIntent = GroupSelectionActiv.getIntent(this, feedPostData);
-        groupSelectionActivIntent.setFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
-        startActivity(groupSelectionActivIntent);
-        finish();
+        //groupSelectionActivIntent.setFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
+        startActivityForResult(groupSelectionActivIntent, REQ_CODE_GROUPS_SELECT);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_CODE_GROUPS_SELECT && resultCode == RESULT_OK) {
+            setResult(RESULT_OK);
+            finish();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
